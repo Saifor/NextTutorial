f5bedd8430b3c78464b6dd9552ea5aafcb386746
Saifor <saifor@bk.ru>
2026-07-01 13:25:48 +0300

    Added banner on main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,14 @@
 "use client"
 
 import Image from "next/image";
+import Banner from "./banner.svg"
 
 export default function Home() {
   return (
     <main className="flex flex-col flex-1 gap-3 p-2 font-sans dark:bg-black/30 border-2 border-dashed dark:border-red-400 border-red-400 bg-red-400/20">
       <span className="text-red-500 font-bold">Page</span>
-      <div className="flex flex-col items-center justify-center w-full">
-        <Image className="dark:invert" src="/banner.png" alt="banner" width={1100} height={100} priority/>
+      <div className="flex flex-col items-center justify-center w-full relative">
+        <Image src={Banner} alt="banner" draggable={false} className="dark:invert select-none pointer-events-none" width={500} height={500} priority/>
       </div>
     </main>
   );
