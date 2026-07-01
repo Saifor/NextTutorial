45f5e13dc00d8f298424a8450d9ae3c39bc10f38
Saifor <saifor@bk.ru>
2026-07-01 14:36:16 +0300

    Design modified

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
     <main className="flex flex-col flex-1 gap-3 p-2 font-sans dark:bg-black/30 border-2 border-dashed dark:border-red-400 border-red-400 bg-red-400/20 select-none">
       <span className="text-red-500 font-bold">Page</span>
       <div className="flex flex-col items-center justify-center w-full relative">
-        <Image src={Banner} alt="banner" draggable={false} className="text-red-500 pointer-events-none" width={500} height={500} priority/>
+        <Image src={Banner} alt="banner" draggable={false} className="dark:invert text-red-500 pointer-events-none" width={500} height={500} priority/>
       </div>
     </main>
   );
