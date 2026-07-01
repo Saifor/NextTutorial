a26579042f05216ce68412f688836df32afa4bb5
Saifor <saifor@bk.ru>
2026-07-01 13:39:09 +0300

    Added banner on main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,10 +5,10 @@ import Banner from "./banner.svg"
 
 export default function Home() {
   return (
-    <main className="flex flex-col flex-1 gap-3 p-2 font-sans dark:bg-black/30 border-2 border-dashed dark:border-red-400 border-red-400 bg-red-400/20">
+    <main className="flex flex-col flex-1 gap-3 p-2 font-sans dark:bg-black/30 border-2 border-dashed dark:border-red-400 border-red-400 bg-red-400/20 select-none">
       <span className="text-red-500 font-bold">Page</span>
       <div className="flex flex-col items-center justify-center w-full relative">
-        <Image src={Banner} alt="banner" draggable={false} className="dark:invert select-none pointer-events-none" width={500} height={500} priority/>
+        <Image src={Banner} alt="banner" draggable={false} className="text-red-500 pointer-events-none" width={500} height={500} priority/>
       </div>
     </main>
   );
